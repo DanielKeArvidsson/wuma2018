206bf3327a26dc4e467a12e992c7ab6bf547b272
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2018-10-09 15:36:04 +0200

    asdf

---
+++ www/js/main.js
@@ -0,0 +1,22 @@
+// console.log("hello world");  1
+
+//let i = "Daniel"; 2
+//console.log("Hello " + i);
+/* let name = prompt("hej vad heter du?"); 
+if (name == 0)  {
+console.log("Good bye unknown stranger"); 
+}
+else {
+console.log("Hello " + name);
+} 
+let i = 10;
+while (i > 0 ) {
+    console.log(i-- + "...");
+}*/
+var i;
+function myfunction(){
+for (i = 0; i < 10; i++) 
+return i;
+console.log(i + "...");
+
+};
