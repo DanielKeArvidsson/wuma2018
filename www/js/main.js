353964cee13330a1d6a648bce6323d5acd06a6c2
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2018-10-09 20:32:52 +0200

    asdf

--- www/js/main.js
+++ www/js/main.js
@@ -12,11 +12,18 @@ console.log("Hello " + name);
 let i = 10;
 while (i > 0 ) {
     console.log(i-- + "...");
-}*/
-var i;
-function myfunction(){
-for (i = 0; i < 10; i++) 
-return i;
-console.log(i + "...");
+}
+let i = [1, 2 , 3, 4, 5, 6, 7, 8, 9];
+function myFunction(){
+let number = " ";   
+ for (number in i);
+return number;
+}
 
-};
+console.log(i);
+
+
+let even = prompt("är ditt tal jämt?");
+let answer = even % 2 === 0;
+alert(answer);
+*/
